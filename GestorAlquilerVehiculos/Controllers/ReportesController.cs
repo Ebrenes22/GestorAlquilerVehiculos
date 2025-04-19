6107c29142caaba892e600f563737b1553ab9e0d
aledenro <133998613+aledenro@users.noreply.github.com>
2025-04-19 16:14:32 -0600

    RF 17, 18, 19

--- GestorAlquilerVehiculos/Controllers/ReportesController.cs
+++ GestorAlquilerVehiculos/Controllers/ReportesController.cs
@@ -1,164 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using GestorAlquilerVehiculos.Data;
+﻿using GestorAlquilerVehiculos.Data;
 using GestorAlquilerVehiculos.Models;
+using GestorAlquilerVehiculos.Utils;
+using Microsoft.AspNetCore.Mvc;
 
-namespace GestorAlquilerVehiculos.Controllers
+public class ReportesController : Controller
 {
-    public class ReportesController : Controller
+    private readonly GestorAlquilerVehiculosDbContext _context;
+
+    public ReportesController(GestorAlquilerVehiculosDbContext context)
     {
-        private readonly GestorAlquilerVehiculosDbContext _context;
+        _context = context;
+    }
 
-        public ReportesController(GestorAlquilerVehiculosDbContext context)
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+
+    public IActionResult PrintReporte(string reportType, DateTime? startDate, DateTime? endDate)
+    {
+        byte[] fileContent;
+        string fileName = $"{reportType}_{DateTime.Now:yyyyMMdd}.xlsx";
+
+        if (reportType == "EstadoFlota")
         {
-            _context = context;
+            var lista = _context.Vehiculos.ToList();
+            var cols = new Dictionary<string, Func<Vehiculo, object>>
+              {
+                  { "Marca", v => v.Marca },
+                  { "Modelo", v => v.Modelo },
+                  { "Placa", v => v.Placa },
+                  { "Estado", v => v.Estado },
+                  { "PrecioPorDia", v => v.PrecioPorDia }
+              };
+            fileContent = ExcelExportService.ExportToExcel<Vehiculo>(lista, cols, "EstadoFlota");
+        }
+        else if (reportType == "Ingresos")
+        {
+            var query = _context.Reservas.AsQueryable();
+            if (startDate.HasValue && endDate.HasValue)
+                query = query.Where(r => r.FechaRegistro >= startDate && r.FechaRegistro <= endDate);
+
+            var lista = query.Select(r => new
+            {
+                Fecha = r.FechaRegistro,
+                CostoTotal = r.CostoTotal,
+                TotalCargos = r.CargosAdicionales.Sum(c => c.Monto)
+            }).ToList();
+
+            var cols = new Dictionary<string, Func<dynamic, object>>
+              {
+                  { "FechaRegistro", x => x.Fecha },
+                  { "CostoTotal", x => x.CostoTotal },
+                  { "TotalCargosAdicionales", x => x.TotalCargos }
+              };
+            fileContent = ExcelExportService.ExportToExcel(lista, cols, "Ingresos");
+        }
+        else
+        {
+            var query = _context.Reservas.AsQueryable();
+            if (startDate.HasValue && endDate.HasValue)
+                query = query.Where(r => r.FechaInicio >= startDate && r.FechaFin <= endDate);
+
+            var lista = query.Select(r => new
+            {
+                r.ReservaID,
+                r.Vehiculo.Placa,
+                Usuario = r.Usuario != null ? r.Usuario.NombreCompleto : r.ClienteReserva.NombreCompleto,
+                r.FechaInicio,
+                r.FechaFin,
+                r.Estado
+            }).ToList();
+
+            var cols = new Dictionary<string, Func<dynamic, object>>
+              {
+                  { "ReservaID", x => x.ReservaID },
+                  { "Placa", x => x.Placa },
+                  { "Usuario/Cliente", x => x.Usuario },
+                  { "FechaInicio", x => x.FechaInicio },
+                  { "FechaFin", x => x.FechaFin },
+                  { "Estado", x => x.Estado }
+              };
+            fileContent = ExcelExportService.ExportToExcel(lista, cols, "Alquileres");
         }
 
-        // GET: Reportes
-        public async Task<IActionResult> Index()
-        {
-            var gestorAlquilerVehiculosDbContext = _context.Reportes.Include(r => r.Usuario);
-            return View(await gestorAlquilerVehiculosDbContext.ToListAsync());
-        }
-
-        // GET: Reportes/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reporte = await _context.Reportes
-                .Include(r => r.Usuario)
-                .FirstOrDefaultAsync(m => m.ReporteID == id);
-            if (reporte == null)
-            {
-                return NotFound();
-            }
-
-            return View(reporte);
-        }
-
-        // GET: Reportes/Create
-        public IActionResult Create()
-        {
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "UsuarioID", "ContrasenaHash");
-            return View();
-        }
-
-        // POST: Reportes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ReporteID,UsuarioID,TipoReporte,FechaGeneracion")] Reporte reporte)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(reporte);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "UsuarioID", "ContrasenaHash", reporte.UsuarioID);
-            return View(reporte);
-        }
-
-        // GET: Reportes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reporte = await _context.Reportes.FindAsync(id);
-            if (reporte == null)
-            {
-                return NotFound();
-            }
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "UsuarioID", "ContrasenaHash", reporte.UsuarioID);
-            return View(reporte);
-        }
-
-        // POST: Reportes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ReporteID,UsuarioID,TipoReporte,FechaGeneracion")] Reporte reporte)
-        {
-            if (id != reporte.ReporteID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(reporte);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ReporteExists(reporte.ReporteID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UsuarioID"] = new SelectList(_context.Usuarios, "UsuarioID", "ContrasenaHash", reporte.UsuarioID);
-            return View(reporte);
-        }
-
-        // GET: Reportes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reporte = await _context.Reportes
-                .Include(r => r.Usuario)
-                .FirstOrDefaultAsync(m => m.ReporteID == id);
-            if (reporte == null)
-            {
-                return NotFound();
-            }
-
-            return View(reporte);
-        }
-
-        // POST: Reportes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var reporte = await _context.Reportes.FindAsync(id);
-            if (reporte != null)
-            {
-                _context.Reportes.Remove(reporte);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ReporteExists(int id)
-        {
-            return _context.Reportes.Any(e => e.ReporteID == id);
-        }
+        return File(
+            fileContent,
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            fileName
+        );
     }
 }
+
+
